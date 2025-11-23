0f568207ee9a6f27cf85b2224821b42ac35f8dee
Shreyas Ashtamkar <shreyu@programmer.net>
2025-11-23 07:37:24 +0530

    refactor: consistency cleanup

--- infra/terraform/modules/gcp_cloudsql/main.tf
+++ infra/terraform/modules/gcp_cloudsql/main.tf
@@ -1,3 +1,58 @@
+#=============================================================================
+# CLOUD SQL MODULE - VARIABLES
+#=============================================================================
+
+variable "project_id" {
+  description = "GCP project ID where Cloud SQL instance will be created"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region for the Cloud SQL instance"
+  type        = string
+}
+
+variable "tier" {
+  description = "Machine tier for Cloud SQL instance (e.g., db-f1-micro, db-custom-1-3840)"
+  type        = string
+}
+
+variable "engine_version" {
+  description = "PostgreSQL engine version"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name of the Cloud SQL instance"
+  type        = string
+}
+
+variable "users_db_name" {
+  description = "Name of the users database"
+  type        = string
+}
+
+variable "submissions_db_name" {
+  description = "Name of the submissions database"
+  type        = string
+}
+
+variable "availability_type" {
+  description = "Availability type (ZONAL or REGIONAL)"
+  type        = string
+  default     = "ZONAL"
+}
+
+variable "ipv4_enabled" {
+  description = "Whether to enable IPv4 (public IP) for the instance"
+  type        = bool
+  default     = false
+}
+
+#=============================================================================
+# CLOUD SQL MODULE - RESOURCES
+#=============================================================================
+
 resource "google_sql_database_instance" "pg" {
   name             = var.instance_name
   database_version = var.engine_version
@@ -23,5 +78,34 @@ resource "google_sql_database" "submissions" {
   name     = var.submissions_db_name
   instance = google_sql_database_instance.pg.name
   project  = var.project_id
+}
+
+#=============================================================================
+# CLOUD SQL MODULE - OUTPUTS
+#=============================================================================
+
+output "connection_name" {
+  description = "Cloud SQL instance connection name (project:region:instance)"
+  value       = google_sql_database_instance.pg.connection_name
+}
+
+output "instance_name" {
+  description = "Cloud SQL instance name"
+  value       = google_sql_database_instance.pg.name
+}
+
+output "users_database_name" {
+  description = "Name of the users database"
+  value       = google_sql_database.users.name
+}
+
+output "submissions_database_name" {
+  description = "Name of the submissions database"
+  value       = google_sql_database.submissions.name
+}
+
+output "self_link" {
+  description = "Self link to the Cloud SQL instance"
+  value       = google_sql_database_instance.pg.self_link
 }
 
--- infra/terraform/modules/gcp_gke/main.tf
+++ infra/terraform/modules/gcp_gke/main.tf
@@ -1,3 +1,61 @@
+#=============================================================================
+# GKE MODULE - VARIABLES
+#=============================================================================
+
+variable "project_id" {
+  description = "GCP project ID where GKE cluster will be created"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region for the GKE cluster"
+  type        = string
+}
+
+variable "zones" {
+  description = "List of zones for multi-zone node pool distribution"
+  type        = list(string)
+}
+
+variable "cluster_name" {
+  description = "Name of the GKE cluster"
+  type        = string
+}
+
+variable "network_name" {
+  description = "VPC network name (null for default network)"
+  type        = string
+  default     = null
+}
+
+variable "subnetwork_name" {
+  description = "Subnetwork name (null for default subnetwork)"
+  type        = string
+  default     = null
+}
+
+variable "node_count" {
+  description = "Number of nodes per zone in the default node pool"
+  type        = number
+  default     = 2
+}
+
+variable "machine_type" {
+  description = "Machine type for GKE nodes"
+  type        = string
+  default     = "e2-standard-4"
+}
+
+variable "environment" {
+  description = "Environment label for nodes"
+  type        = string
+  default     = "dev"
+}
+
+#=============================================================================
+# GKE MODULE - RESOURCES
+#=============================================================================
+
 resource "google_container_cluster" "primary" {
   name                     = var.cluster_name
   location                 = var.region
@@ -25,5 +83,36 @@ resource "google_container_node_pool" "default" {
     oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
     labels       = { environment = var.environment }
   }
+}
+
+#=============================================================================
+# GKE MODULE - OUTPUTS
+#=============================================================================
+
+output "name" {
+  description = "GKE cluster name"
+  value       = google_container_cluster.primary.name
+}
+
+output "endpoint" {
+  description = "GKE cluster endpoint (API server URL)"
+  value       = google_container_cluster.primary.endpoint
+  sensitive   = true
+}
+
+output "ca_certificate" {
+  description = "Base64 encoded cluster CA certificate"
+  value       = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+  sensitive   = true
+}
+
+output "location" {
+  description = "GKE cluster location (region or zone)"
+  value       = google_container_cluster.primary.location
+}
+
+output "cluster_id" {
+  description = "GKE cluster unique identifier"
+  value       = google_container_cluster.primary.id
 }
 
